4580d90ad108c3bca99ad4914b2d8d21a64a7a0a
Francis Pion <francispion@hotmail.com>
2026-04-15 12:31:18 -0400

    Initial commit.

---
+++ vite.config.ts
@@ -0,0 +1,15 @@
+import vue from "@vitejs/plugin-vue";
+import vueDevTools from "vite-plugin-vue-devtools";
+import vueJsx from "@vitejs/plugin-vue-jsx";
+import { defineConfig } from "vite";
+import { fileURLToPath, URL } from "node:url";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [vue(), vueJsx(), vueDevTools()],
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+});
